7a1b59a0be5bcb73e067b8596f861f56dc522878
Yusmenov <erkan.karaduman93@gmail.com>
2022-04-11 23:48:06 +0300

    Design changes

--- app/src/main/java/com/erkankaraduman/fenogretmenm/fragments/LoginFragment.kt
+++ app/src/main/java/com/erkankaraduman/fenogretmenm/fragments/LoginFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.erkankaraduman.fenogretmenm.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
@@ -15,6 +18,14 @@ class LoginFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initView()
 
-
+    }
+    private fun initView(){
+    tw_signup.setOnClickListener{
+        findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+    }
+    }
 }
